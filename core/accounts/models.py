e18d1f58c2c5d7072081c5488d14c56dab5389c6
Farimah <108592778+farimah-tizghadam@users.noreply.github.com>
2026-07-08 17:44:57 +0330

    complete admin for accounts and blog app
    + commit blog and admin
    + custom admin forms for both accounts and blog
    + add media and static construct

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.utils.translation import gettext_lazy as _
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    '''
+    custom user model manager where email is the unique identifiers for 
+    authentication instead of username.
+    '''
+    
+    def create_user(self,email,password,**extra_fields):
+        '''
+        create and save the user with the given password and extra data
+        '''
+
+        if not email:
+            raise ValueError(_("email should't be empty"))
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self,email,password,**extra_fields):
+        '''
+        create and save a superuser with the given data.
+        '''
+        
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''
+    custom user model
+    '''
+    email = models.EmailField(max_length=225, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=20)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+    
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+    
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post,Category
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'status', 'category', 'created_date', 'published_date']
+
+
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Post)
